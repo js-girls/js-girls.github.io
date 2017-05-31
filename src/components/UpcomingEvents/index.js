4380d9af07afa84f2d1ad0ff108068ccb52d5500
Jaga Santagostino <jagasantagostino@gmail.com>
2017-05-31 19:29:52 +0200

    add link event

--- src/components/UpcomingEvents/index.js
+++ src/components/UpcomingEvents/index.js
@@ -1,37 +1,46 @@
-import React, {PropTypes} from "react"
-import {Link} from 'phenomic'
+import React, { PropTypes } from "react"
 import styles from './index.css'
 
 const UpcomingEvents = (props) => {
 
-    return (
-        <div className={styles.wrapper}>
-            <h3 className={styles.title}>Upcoming Events</h3>
-            <div>
-                {
-                    props.events.map(event => {
-                        const EventComponent = event.disableLink ? 'div' : Link
-                        return (
-                            <EventComponent key={event.name} className={styles.event} to={event.name}>
-                                    <div className={styles.location}>{event.location}</div>
-                                    <div className={styles.date}>{event.date}</div>
-                            </EventComponent>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.wrapper}>
+      <h3 className={styles.title}>Upcoming Events</h3>
+
+      <div>
+        {
+          props.events.map(event => {
+            if (event.url) {
+              return (
+                <div  key={event.name} className={styles.event}>
+                  <div className={styles.location}>{event.location}</div>
+                  <div className={styles.date}>{event.date}</div>
+                  <a target="_blank" href={event.url} className={styles.cta}>Get Ticket</a>
+                </div>
+              );
+            }
+
+            return (
+              <div key={event.name} className={styles.event}>
+                <div className={styles.location}>{event.location}</div>
+                <div className={styles.date}>{event.date}</div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>
+  )
 }
 
 UpcomingEvents.propTypes = {
-    events: PropTypes.arrayOf(
-        PropTypes.shape({
-            name: PropTypes.string.isRequired,
-            location: PropTypes.string.isRequired,
-            date: PropTypes.string.isRequired
-        })
-    ).isRequired
+  events: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      date: PropTypes.string.isRequired
+    })
+  ).isRequired
 }
 
 export default UpcomingEvents
--- src/events.js
+++ src/events.js
@@ -2,15 +2,14 @@ const events = [
     {
         name: 'roma-2017',
         location: 'Rome',
-        date:  'to be announced',
-        disableLink: true
+        date:  'sabato 10 giugno',
+        url: 'https://www.eventbrite.it/e/biglietti-js-girls-roma-34895610711?aff=eand'
     },
 
     {
         name: 'verona-2017',
         location: 'Verona',
         date:  'to be announced',
-        disableLink: true
     }
 ]
 
