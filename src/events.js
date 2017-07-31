a4aaa146772939d29c1e9c6615e96b2bb24912b6
Sommavilla, Thomas <thomas.sommavilla@accenture.com>
2017-07-31 22:39:21 +0200

    [Thomas] Past event deleted

--- src/events.js
+++ src/events.js
@@ -1,16 +1,9 @@
 const events = [
     {
-        name: 'roma-2017',
-        location: 'Rome',
-        date:  'sabato 10 giugno',
-        url: 'https://www.eventbrite.it/e/biglietti-js-girls-roma-34895610711?aff=eand'
-    },
-
-    {
         name: 'verona-2017',
         location: 'Verona',
         date:  'to be announced',
     }
-]
+];
 
 export default events
